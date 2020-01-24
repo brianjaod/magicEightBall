17f11aca88298aab68c630082a28de5163f29a39
Brian Jones <brianj@alphaonedesign.com>
2020-01-24 12:08:46 -0700

    Add random pic and delay logic, plus startup delay time change.

--- Magic Eight Ball/ViewController.swift
+++ Magic Eight Ball/ViewController.swift
@@ -12,13 +12,23 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var ballPic: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+    let ballArray = [#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball5")]
+    
     @IBAction func askButtonPressed(_ sender: UIButton) {
         print("Ask button pressed")
+        ballPic.image = ballArray.randomElement()        
+        // default back to ? pic after delay
+        delayWithSeconds(3){
+            self.ballPic.image = #imageLiteral(resourceName: "ball0")
+        }
+        
+    }
+    
+    
+    func delayWithSeconds(_ seconds: Double, completion: @escaping () -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            completion()
+        }
     }
     
 }
